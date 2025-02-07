798044bdbb25e2d058a1b0f4397583e4c3452873
Adones Balcia <adonesbalica@gmail.com>
2025-02-07 16:45:27 -0300

    Add controls to Logo stories

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,7 +1,13 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Logo from '.'
+import Logo, { LogoProps } from '.'
+
 export default {
   title: 'Logo',
   component: Logo
 } as Meta
-export const Default: StoryObj = {}
+
+export const Default: StoryObj<LogoProps> = {
+  args: {
+    color: 'white'
+  }
+}

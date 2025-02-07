86f7bdd9a331286630e462bb4b75467873274b2a
Adones Balcia <adonesbalica@gmail.com>
2025-02-07 17:07:54 -0300

    Add color tests to Logo

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -4,5 +4,6 @@ import { render, RenderResult } from '@testing-library/react'
 import theme from '@/styles/theme'
 import React from 'react'
 
-export const renderWithTheme = (children: React.ReactNode): RenderResult =>
-  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
+export const renderWithTheme = (children: React.ReactNode): RenderResult => {
+  return render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
+}

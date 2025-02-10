20596abbcf20213a493b0cb4c2e9e318415d1ebc
Adones Balcia <adonesbalica@gmail.com>
2025-02-10 16:22:07 -0300

    Add animations to menu

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -90,6 +90,7 @@ export const MenuFull = styled.nav<MenuProps>`
     right: 0;
     height: 100vh;
     overflow: hidden;
+    transition: opacity 0.3s ease-in-out;
     opacity: ${isOpen ? 1 : 0};
     pointer-events: ${isOpen ? 'all' : 'none'};
 
@@ -115,7 +116,13 @@ export const MenuFull = styled.nav<MenuProps>`
       color: ${theme.colors.black};
       font-weight: ${theme.font.bold};
       font-size: ${theme.font.sizes.xlarge};
-      margin-bottom: ${theme.spacings.small};
+      transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
+    }
+
+    ${RegisterBox} {
+      transform: ${isOpen ? 'translateY(0)' : 'translateY(3rem)'};
+      transition: transform 0.3s ease-in-out;
     }
   `}
 `;
